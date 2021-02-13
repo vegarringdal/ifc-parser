fe8ddacbc73fe1a30dbc5cbe8bd037ebef6c1dba
Vegar Ringdal <vegar.ringdal@gmail.com>
2021-02-13 19:21:35 +0100

    chore, just keep main

--- src_client/index.ts
+++ src_client/index.ts
@@ -13,20 +13,31 @@ import("./app-root").then(() => {
 });
 
 export const readerWorker = new Worker("readerWorker.js");
-let x = [];
+
+let keep: any = [];
+
 readerWorker.addEventListener("message", (e) => {
   if (e.data === "start") {
-    console.time("back");
+    keep = [];
+    console.time("back, this is total time..");
   }
   if (e.data === "done") {
-    console.timeEnd("back");
-    console.log("array length:", x.length);
-    console.log("row:", x[500]);
-    console.log("row:", x[1500]);
-    console.log("row:", x[2500]);
-    console.log("row:", x[4500]);
+    console.timeEnd("back, this is total time..");
+    keep.forEach((e) => {
+      console.log(e);
+    });
   }
-  if (e.data?.length) {
-    x = x.concat(e.data);
+  if (Array.isArray(e.data)) {
+    e.data.forEach((r: any) => {
+      if (
+        r[1] === "IFCPROJECT" ||
+        r[1] === "IFCSITE" ||
+        r[1] === "IFCBUILDING" ||
+        r[1] === "IFCBUILDINGSTORY" ||
+        r[1] === "IFCSPACES"
+      ) {
+        keep.push(r);
+      }
+    });
   }
 });
