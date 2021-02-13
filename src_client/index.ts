4e36083879ea6651cb7b281d5ec5aa4897de6cc7
Vegar Ringdal <vegar.ringdal@gmail.com>
2021-02-13 20:03:15 +0100

    save

--- src_client/index.ts
+++ src_client/index.ts
@@ -17,6 +17,20 @@ export const readerWorker = new Worker("readerWorker.js");
 let keep: any = [];
 
 readerWorker.addEventListener("message", (e) => {
+
+  /*
+  I send back parsed data evry 25 k rows
+  maybe I should send this to new worker?
+
+  I should build up map so we know what each item of array is
+  first is always ID then NAME
+  but rest we need a map for.
+  then we can wrap it in a js proxy to save memeory
+  make json will kill unit using IFC.js
+
+  */
+
+
   if (e.data === "start") {
     keep = [];
     console.time("back, this is total time..");
