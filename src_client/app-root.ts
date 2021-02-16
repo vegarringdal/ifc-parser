c5c1493f1a090fdd434bdb39e7102e320b5054bd
Vegar Ringdal <vegar.ringdal@gmail.com>
2021-02-16 19:26:45 +0100

    empty project

--- src_client/app-root.ts
+++ src_client/app-root.ts
@@ -1,10 +1,8 @@
-import { html } from "lit-html";
-import { customElement } from "@simple-html/core";
-import "./ifc-reader/ifc-reader";
-
-@customElement("app-root")
-export default class extends HTMLElement {
-  public render() {
-    return html` <ifc-reader class="p-2"></ifc-reader> `;
+export class AppRoot extends HTMLElement {
+  connectedCallback(){
+    this.innerHTML = 'hello world'
   }
+
 }
+
+customElements.define("app-root", AppRoot);
--- src_client/index.ts
+++ src_client/index.ts
@@ -1,14 +1,1 @@
-// only use hmr if development
-declare let DEVELOPMENT: boolean;
-if (DEVELOPMENT) {
-  const { applyPolyfill } = require("custom-elements-hmr-polyfill");
-  applyPolyfill();
-}
-
-// load out elements
-import("./app-root").then(() => {
-  if (document.body) {
-    document.body.innerHTML = "<app-root></app-root>";
-  }
-});
-
+import "./app-root";
